46a83f8a40b693edb159aa8635bff39dcabb4ed1
Bereket Abraham <babraham42@gmail.com>
2016-10-15 13:59:51 -0400

    localizer analysis code

---
+++ get_logs.m
@@ -0,0 +1,8 @@
+% Read and transform messages logs from a buggy roll
+% Bereket Abraham
+
+% addpath('./jsonlab');
+% data = loadjson('../rolls_logs/2016-10-09-06-17-15/sensors_2016-10-09-06-17-15.txt');
+% data = loadjson('../rolls_logs/sample.txt');
+
+load('logs.mat');
